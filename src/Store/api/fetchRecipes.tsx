84b9ae5c1ad3f744a65e8a453bf1285617e66b95
Yuni <newnny0812@gmail.com>
2024-02-05 15:13:44 +0100

    fixing deployment api issue

--- src/Store/api/fetchRecipes.tsx
+++ src/Store/api/fetchRecipes.tsx
@@ -29,10 +29,11 @@ interface ogReceipes {
   }[];
 }
 
+const apiUrl: string = "https://yummly2.p.rapidapi.com/feeds/search";
 const apiKey: string = process.env.REACT_APP_YUMMLY_API_KEY as string;
 
 export const fetchRecipes = createAsyncThunk("recipes/fetch", async (ingredient: string, thunkAPI) => {
-  const res = await fetch(`https://yummly2.p.rapidapi.com/feeds/search?start=0&maxResult=20&start=10&q=${ingredient}`, {
+  const res = await fetch(`${apiUrl}?start=0&maxResult=20&start=10&q=${ingredient}`, {
     method: "GET",
     headers: {
       'X-RapidAPI-Key': apiKey,
